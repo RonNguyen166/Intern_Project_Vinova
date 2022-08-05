0001b094312f9f1af08871b7bf55d4606b98a37c
hungpd90 <eme2965@gmail.com>
2022-08-05 17:06:16 +0700

    save

--- src/modules/Posts/post.route.ts
+++ src/modules/Posts/post.route.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import CommentController from "../Comments/comment.controller";
 import PostController from "./post.controller";
-import {auth} from "./../../middlewares/authen.middleware";
+import {isAuthor, isAuthen} from "./../../middlewares/authen.middleware";
+import { upload } from "../../common/services/upload2.service";
 
 const commentController = new CommentController();
 const postController = new PostController();
@@ -11,7 +12,7 @@ const router = express.Router();
 router
   .route("/")
   .get(postController.getAllPost)
-  .post(auth("member", "admin"), postController.createPost);
+  .post(isAuthen, isAuthor, upload.single("photo"),postController.createPost);
 router.route("/:postId/comments").get(commentController.getCommentsByPostId);
 
 router
